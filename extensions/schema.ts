27c04d1f674a2e058776f263812c6f5f9aaf8f8d
Matilde Park <matilde@park.net>
2024-04-09 20:18:40 -0400

    extensions: example schema

--- extensions/schema.ts
+++ extensions/schema.ts
@@ -1,1 +1,7 @@
 // Expand the schema of how responses should look.
+import { ModelResponseSchema } from "nolita/dist/types/browser/actionStep.types";
+import { z } from "zod";
+
+export const CustomSchema = ModelResponseSchema.extend({
+    restaurants: z.array(z.string().optional().describe("The name of a restaurant")),
+})
