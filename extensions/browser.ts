d5124c76eab7edc716e7e80763356a9fb8ad534e
Matilde Park <matilde@park.net>
2024-04-09 20:18:36 -0400

    extensions: put browser into server direct for now

--- extensions/browser.ts
+++
@@ -1,5 +0,0 @@
-import { Browser } from "nolita";
-
-const browser = Browser.create(true);
-
-export default browser;
--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 import path from 'path';
 import agent from '../agent';
-import browser from '../extensions/browser';
-import { AgentBrowser, Logger } from 'nolita';
+import { Browser, AgentBrowser, Logger } from 'nolita';
 import { ModelResponseSchema } from 'nolita/dist/types/browser/actionStep.types';
 import inventory from '../extensions/inventory';
+import { CustomSchema } from '../extensions/schema';
 
 const app = express();
 const port = 3040;
@@ -14,14 +14,29 @@ app.listen(port, () => {
     console.log(`Server started on http://localhost:${port}`);
 });
 
+const cors = {
+    origin: ["http://localhost:5173"],
+    default: "http://localhost:5173"
+  }
+  
+app.all('*', function(req, res, next) {
+    const origin = cors.origin.includes(req.header('origin').toLowerCase()) ? req.headers.origin : cors.default;
+    res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 // In prod, serve the front-end
 if (process.env.NODE_ENV === 'production') {
     app.use('/', express.static(path.join(__dirname, '../app/dist')));
 }
 
-app.post('/api/browse', async (req, res) => {
+app.get('/api/browse', async (req, res) => {
     res.setHeader('Content-Type', 'text/event-stream');
-    const logger = new Logger("info", (msg) => res.write(msg));
+    const browser = await Browser.create(true);
+    const logger = new Logger("info", (msg) => {
+        return res.write(`data: ${msg}\n\n`);
+    });
     const agentBrowser = new AgentBrowser(
         agent,
         browser,
@@ -31,13 +46,18 @@ app.post('/api/browse', async (req, res) => {
     const answer = await agentBrowser.browse({
         startUrl: req.query.url as string,
         objective: [req.query.objective as string],
-        maxIterations: req.query.maxIterations as number || 10
+        maxIterations: parseInt(req.query.maxIterations as string) || 10
     },
-    ModelResponseSchema
+    CustomSchema
     );
     await agentBrowser.close();
     if (answer) {
         res.write(`data: ${JSON.stringify(answer)}\n\n`);
-        res.end();
+        res.write(`data: {"done": true}\n\n`);
+        return res.end();
+    } else {
+        res.write(`data: {"error": "no answer found"}\n\n`);
+        res.write(`data: {"done": true}\n\n`);
+        return res.end();
     }
 });
