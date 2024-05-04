9f2166841a1b3ae6c096572d1f1d17b33c5f8451
Matilde Park <matilde.park@gmail.com>
2024-05-04 13:11:55 -0400

    pkg: pull out app package.json and merge into parent

--- extensions/inventory.ts
+++ extensions/inventory.ts
@@ -1,6 +1,11 @@
 import { Inventory } from "nolita";
 import { readFileSync } from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 const inventoryObj = JSON.parse(
   readFileSync(path.join(__dirname, ".inventory"), "utf8"),
